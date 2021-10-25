ce8629e51ac18ad64341648ca1b9a2f9f1fc7c81
GangadharNeelam <93145713+GangadharNeelam@users.noreply.github.com>
2021-10-26 00:14:23 +0530

    Add files via upload

---
+++ Back slash t and n.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	printf("student \tInitial\tBranch    \tCGPA\n");
+	printf("Gangaram\tNeelam \tMechanical\t10.0\n");
+	printf("Subbu   \tPadala \tMechanical\t10.0");
+}
